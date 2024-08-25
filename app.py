26dd459c72a16e1cb88dc5e74a23fa0f631246ea
Anton Garay <antonvince.garay@gmail.com>
2024-08-25 22:32:08 +1200

    Added options to change type and quality of image when converting

--- app.py
+++ app.py
@@ -1,56 +1,44 @@
-from io import BytesIO
+import io
+import os
 
 from flask import Flask, render_template, request, send_file
-from PIL import Image, UnidentifiedImageError
+from PIL import Image
 
 app = Flask(__name__)
 
-# Set maximum file size (e.g., 5MB)
-MAX_FILE_SIZE = 5 * 1024 * 1024  # 5 Megabytes
-
 
 @app.route("/", methods=["GET", "POST"])
-def convert_image():
+def upload_file():
     if request.method == "POST":
-        if "file" not in request.files:
-            return "No file uploaded", 400
         file = request.files["file"]
+        format = request.form.get("format", "webp").lower()
+        quality = int(request.form.get("quality", 80))
 
-        # Check if the file has been selected
-        if file.filename == "":
-            return "No file selected", 400
+        if file:
+            image = Image.open(file.stream)
 
-        # Check if the file size is within the limit
-        if file.content_length > MAX_FILE_SIZE:
-            return "File size exceeds the maximum limit of 5 MB", 400
+            output_io = io.BytesIO()
+            save_kwargs = {}
 
-        try:
-            # Attempt to open the file as an image
-            input_image = Image.open(file.stream)
-            input_image.verify()  # Verify that this is an actual image
-            file.stream.seek(0)  # Reset stream position to the beginning
+            if format in ["jpeg", "webp"]:
+                save_kwargs["quality"] = quality
 
-            # Convert to WebP
-            img_io = BytesIO()
-            input_image = Image.open(file.stream)  # Reopen image to avoid verify mode
-            input_image.save(img_io, "WEBP", optimize=True, quality=10)
-            img_io.seek(0)
+            image.save(output_io, format=format.upper(), **save_kwargs)
+            output_io.seek(0)
 
-            # Send the WebP image back to the user
+            original_filename = file.filename
+            basename, ext = os.path.splitext(original_filename)
+            new_filename = f"{basename}_converted.{format}"
+
             return send_file(
-                img_io,
-                mimetype="image/webp",
-                as_attachment=True,
-                download_name="converted_image.webp",
+                output_io,
+                mimetype=f"image/{format}",
+                download_name=new_filename,
+                as_attachment=True,  # Ensure the file is downloaded
             )
 
-        except UnidentifiedImageError:
-            return "The uploaded file is not a valid image", 400
-        except Exception as e:
-            return str(e), 500
-
-    return render_template("test.html")
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=5100)
+    app.run(debug=True)
